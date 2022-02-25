fa820645095749abe3e1956915df938fe6c71ad3
munimadugusathish <95748044+munimadugusathish@users.noreply.github.com>
2022-02-25 08:57:50 +0530

    jwr4
    kw;huj

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Satheesh, Wellcome to Our World!
+          Hi , Wellcome to Our World!
         </p>
     
       </header>
